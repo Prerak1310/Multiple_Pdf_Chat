dc0e7976b14f8d26228898a171760802c4de0141
Prerak1310 <preraksangwan@gmail.com>
2024-06-28 17:42:40 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-# __import__("pysqlite3")
-# import sys
+__import__("pysqlite3")
+import sys
 
-# sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 import os
 from dotenv import load_dotenv
